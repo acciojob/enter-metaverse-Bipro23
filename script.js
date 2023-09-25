0df9c9c6ff57871a582e6644b80e12621d3cecbc
Bipro23 <88981554+Bipro23@users.noreply.github.com>
2023-09-25 19:20:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// Get references to the HTML elements
+const statusParagraph = document.getElementById("status");
+const enterButton = document.getElementById("enterBtn");
+const outputHeading = document.getElementById("output");
+
+// Add a click event listener to the button
+enterButton.addEventListener("click", function() {
+    // Change the text content of the paragraph
+    statusParagraph.textContent = "Entered Metaverse";
+    
+    statusParagraph.appendChild(outputHeading);
+    outputHeading.textContent = "Entered Metaverse";
+});
